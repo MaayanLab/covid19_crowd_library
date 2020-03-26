6f10af79d909cb1b91ca786bdbb03c8171d4f8a1
Maxim Kuleshov <kuleshov.max.v@gmail.com>
2020-03-26 18:31:28 -0400

    Showing/hiding contact details

--- app/static/enrichr.js
+++ app/static/enrichr.js
@@ -1,19 +1,37 @@
-function DTblify(json) {
+function DTblify(json, reviewed) {
     let dataArray = [];
     for (let i = 0; i < json.length; i++) {
         // Links to enriched genes
         let enrichedLinks = [];
-        $.each(json[i]['genes'].split('\n'), function (index, gene) {
+        $.each(json[i]['genes'].split('\n').sort(), function (index, gene) {
             enrichedLinks.push('<a class="enriched-gene-link" href="http://amp.pharm.mssm.edu/Harmonizome/gene/' + gene + '" target="_blank">' + gene + '</a>');
         });
+        const showContacts = json[i]['showContacts'];
+        const contacts = showContacts ? `<p><b>Author:</b> ${json[i]['authorName']}<\p><p><b>Affiliation:</b> ${json[i]['authorAffiliation']}<\p><p><b>E-mail:</b> ${json[i]['authorEmail']}<\p>` :
+            reviewed ? '<i class="far fa-eye-slash"></i> Author preferred not to share contact details' : `<p style="color: red"><i class="far fa-eye-slash"></i> As author preferred not to share contact details, following will not be shown</p><p style="color: red"><b>Author:</b> ${json[i]['authorName']}<\p><p style="color: red"><b>Affiliation:</b> ${json[i]['authorAffiliation']}<\p><p><b>E-mail:</b> ${json[i]['authorEmail']}<\p>`;
+        let desc = `<p>${json[i]['descrFull']}<\p>${contacts}`;
+        console.log(showContacts);
+        console.log(contacts);
+        console.log(desc);
 
         // Data Array
         dataArray[i] = [
-            json[i]['descrShort'],
             $('<div>', {
                 'class': 'enrichment-popover-button',
                 'data-toggle': 'popover',
-                'data-placement': 'left',
+                'data-placement': 'bottom',
+                'data-html': 'true',
+                'data-template': '<div class="popover enrichment-popover" role="tooltip"><div class="arrow"></div><h3 class="popover-header"></h3><div class="popover-body"></div></div>',
+                'data-content': desc,
+                'title': json[i]['descrShort']
+            })
+                .append(`<span style="cursor: pointer;text-decoration: underline dotted;">${json[i]['descrShort']}</span>`)
+                .prop('outerHTML'),
+            $('<div>', {
+                'class': 'enrichment-popover-button',
+                'data-toggle': 'popover',
+                'data-placement': 'bottom',
+                // 'data-trigger': 'focus',
                 'data-html': 'true',
                 'data-template': '<div class="popover enrichment-popover" role="tooltip"><div class="arrow"></div><h3 class="popover-header"></h3><div class="popover-body"></div></div>',
                 'title': 'Gene set',
@@ -32,7 +50,7 @@ function drawTable(reviewed) {
         $.get("enrichr", {reviewed: reviewed}, function (data) {
         $('#enrichr_table').DataTable({
             width: '100%',
-            data: DTblify(JSON.parse(data)),
+            data: DTblify(JSON.parse(data), reviewed),
             responsive: true,
             columns: [
                 {title: "Description"},
