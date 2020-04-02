6a401f6ea4b400cada6d76e7663c4c677c38df0f
Daniel J. B. Clarke <u8sand@gmail.com>
2020-04-02 11:23:26 -0400

    Added convenient database helper object_as_dict

--- app/database.py
+++ app/database.py
@@ -1,5 +1,5 @@
 import os
-from sqlalchemy import create_engine
+import sqlalchemy as sa
 from sqlalchemy.orm.session import sessionmaker
 
 user = os.environ.get('DB_USER')
@@ -11,9 +11,15 @@ if user or password or host or db:
   logging.warn('DB, DB_USER, DB_PASSWORD, and HOST are deprecated, please use DB_URI (mysql+pymysql://{USER}:{PASS}@{HOST}/{DB})')
 db_uri = os.environ.get('DB_URI', 'mysql+pymysql://{0}:{1}@{2}/{3}'.format(user, password, host, db))
 
-engine = create_engine(db_uri, pool_recycle=300)
+engine = sa.create_engine(db_uri, pool_recycle=300)
 Session = sessionmaker(bind=engine)
 
 def init():
   from app.models import Base
   Base.metadata.create_all(engine)
+
+def object_as_dict(obj):
+  return {
+    c.key: getattr(obj, c.key)
+    for c in sa.inspect(obj).mapper.column_attrs
+  }
