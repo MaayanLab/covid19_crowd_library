37b3d7106724b9a13647330420d061c610900061
Daniel J. B. Clarke <u8sand@gmail.com>
2020-04-03 18:35:31 -0400

    Only include reviewed items in stats/downloads
    Fixes #69

--- app/download.py
+++ app/download.py
@@ -3,12 +3,12 @@ from app.models import Geneset, Drugset
 
 def genesets():
   sess = Session()
-  for geneset in sess.query(Geneset):
+  for geneset in sess.query(Geneset).filter(Geneset.reviewed == 1):
     yield geneset.to_gmt() + '\n'
   sess.close()
 
 def drugsets():
   sess = Session()
-  for drugset in sess.query(Drugset):
+  for drugset in sess.query(Drugset).filter(Drugset.reviewed == 1):
     yield drugset.to_gmt() + '\n'
   sess.close()
--- app/statistics.py
+++ app/statistics.py
@@ -3,34 +3,45 @@ from app.database import Session
 from app.models import Geneset, GenesetGene, Gene, Drugset, DrugsetDrug, Drug
 from app.datatables import serve_datatable
 
+gene_count = sa.func.count(Gene.id)
+drug_count = sa.func.count(Drug.id)
+
 def stats():
   sess = Session()
   ret = {
-    'n_genesets': sess.query(Geneset).count(),
-    'n_drugsets': sess.query(Drugset).count(),
-    'n_unique_genes': sess.query(Gene).count(),
-    'n_unique_drugs': sess.query(Drug).count(),
+    'n_genesets': sess.query(Geneset).filter(Geneset.reviewed == 1).count(),
+    'n_drugsets': sess.query(Drugset).filter(Drugset.reviewed == 1).count(),
+    'n_unique_genes': sess.query(Gene).distinct(Gene.id) \
+      .join(GenesetGene, GenesetGene.gene == Gene.id) \
+      .join(Geneset, GenesetGene.geneset == Geneset.id) \
+      .filter(Geneset.reviewed == 1) \
+      .count(),
+    'n_unique_drugs': sess.query(Drug).distinct(Drug.id) \
+      .join(DrugsetDrug, DrugsetDrug.drug == Drug.id) \
+      .join(Drugset, DrugsetDrug.drugset == Drugset.id) \
+      .filter(Drugset.reviewed == 1) \
+      .count(),
   }
   sess.close()
   return ret
 
 
-gene_count = sa.func.count(Gene.id)
 def top_genes_qs(sess):
   return sess.query(Gene.symbol, gene_count) \
     .join(GenesetGene, GenesetGene.gene == Gene.id) \
     .join(Geneset, GenesetGene.geneset == Geneset.id) \
+    .filter(Geneset.reviewed == 1) \
     .group_by(Gene.id)
 def top_genes_search(val):
   return Gene.symbol.like(f'%{val}%')
 top_genes = serve_datatable(top_genes_qs, [(Gene.symbol, 'symbol'), (gene_count, 'count')], top_genes_search)
 
 
-drug_count = sa.func.count(Drug.id)
 def top_drugs_qs(sess):
   return sess.query(Drug.symbol, drug_count) \
     .join(DrugsetDrug, DrugsetDrug.drug == Drug.id) \
     .join(Drugset, DrugsetDrug.drugset == Drugset.id) \
+    .filter(Drugset.reviewed == 1) \
     .group_by(Drug.id)
 def top_drugs_search(val):
   return Drug.symbol.like(f'%{val}%')
