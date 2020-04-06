e4c68d4eef210d488b832095879d3e9613d5eed9
Daniel J. B. Clarke <u8sand@gmail.com>
2020-04-06 11:49:45 -0400

    Improve tab navigation with anchors
    References #74

--- app/static/index.js
+++ app/static/index.js
@@ -1,12 +1,16 @@
-function navTabsSync() {
-    $(".genes").on("click", function () {
-        console.log('genes click');
-        $(".drugs").removeClass("active").removeClass("show");
-        $(".genes").addClass("active").addClass("show");
-    });
-    $(".drugs").on("click", function () {
-        console.log('drugs click');
-        $(".genes").removeClass("active").removeClass("show");
-        $(".drugs").addClass("active").addClass("show");
+function navTabsSync(def) {
+    if (location.hash !== '') {
+        $('a[href="' + location.hash + '"]').tab('show');
+    } else {
+        $('a[href="' + def + '"]').tab('show');
+    }
+
+    $("a[data-toggle='tab']").on("shown.bs.tab", function (e) {
+        var hash = $(e.target).attr("href");
+        if (hash.substr(0, 1) == "#") {
+            var position = $(window).scrollTop();
+            location.replace("#" + hash.substr(1));
+            $(window).scrollTop(position);
+        }
     });
 }
