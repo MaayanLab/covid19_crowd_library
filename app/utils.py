965179014ae0afbe6894db80e12a0139ab967adb
Maxim Kuleshov <kuleshov.max.v@gmail.com>
2020-04-09 14:06:40 -0400

    Close #86

--- app/utils.py
+++ app/utils.py
@@ -3,6 +3,6 @@ def match_meta(form, max_form_len):
     meta = dict()
     for i in range(n_fields):
         if 'name_{0}'.format(i) in form:
-            meta[form['name_{0}'.format(i)]] = form['val_{0}'.format(i)]
-    print(meta)
+            if form['name_{0}'.format(i)].strip() != '':
+                meta[form['name_{0}'.format(i)]] = form['val_{0}'.format(i)]
     return meta
