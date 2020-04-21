ea99a1b04d12eea24a1d5d99ac18ceb1db71aea1
Maxim Kuleshov <kuleshov.max.v@gmail.com>
2020-04-21 11:38:06 -0400

    Proper test

--- app/tests/test_drugsets.py
+++ app/tests/test_drugsets.py
@@ -2,20 +2,21 @@ import requests
 import os
 from datetime import datetime
 from pytz import timezone
+from app import drugset
 
 ROOT_PATH = os.environ.get('ROOT_PATH', '/covid19/')
 
 
 class TestDrugsets:
-    def test_submit(self):
-        url = 'http://0.0.0.0:8080/covid19/drugsets'
-        payload = {'source': 'https://github.com/MaayanLab/COVID19DrugsTrendTracker/blob/master/daily_reports',
-                   'drugSet': open('drug_list.txt').read(),
-                   'descrShort': 'Test: Standard submission {0}'.format(datetime.now(timezone('US/Eastern')).strftime('%Y-%m-%d %H:%M:%S')),
-                   'descrFull': '',
-                   'authorName': 'PyTest',
-                   'authorEmail': 'test@mail.com',
-                   'authorAff': 'PyTest'}
+    form = {'source': 'https://github.com/MaayanLab/COVID19DrugsTrendTracker/blob/master/daily_reports',
+            'drugSet': open('drug_list.txt').read(),
+            'descrShort': 'Test: Standard submission {0}'.format(
+                datetime.now(timezone('US/Eastern')).strftime('%Y-%m-%d %H:%M:%S')),
+            'descrFull': '',
+            'authorName': 'PyTest',
+            'authorEmail': 'test@mail.com',
+            'authorAff': 'PyTest'}
 
-        response = requests.request("POST", url, data=payload)
-        assert response.status_code == 200
+    def test_add_drugset(self):
+        response = drugset.add_drugset(self.form)
+        assert response == ('{"success": true}', 200, {'ContentType': 'application/json'})
--- app/tests/test_genesets.py
+++ app/tests/test_genesets.py
@@ -2,20 +2,21 @@ import requests
 import os
 from datetime import datetime
 from pytz import timezone
+from app import geneset
 
 ROOT_PATH = os.environ.get('ROOT_PATH', '/covid19/')
 
 
 class TestGenesets:
-    def test_submit(self):
-        url = 'http://0.0.0.0:8080/covid19/genesets'
-        payload = {'source': 'https://amp.pharm.mssm.edu/Enrichr',
-                   'geneSet': open('gene_list.txt').read(),
-                   'descrShort': 'Test: Standard submission {}'.format(datetime.now(timezone('US/Eastern')).strftime('%Y-%m-%d %H:%M:%S')),
-                   'descrFull': '',
-                   'authorName': 'PyTest',
-                   'authorEmail': 'test@mail.com',
-                   'authorAff': 'PyTest'}
+    form = {'source': 'https://amp.pharm.mssm.edu/Enrichr',
+            'geneSet': open('gene_list.txt').read(),
+            'descrShort': 'Test: Standard submission {}'.format(
+                datetime.now(timezone('US/Eastern')).strftime('%Y-%m-%d %H:%M:%S')),
+            'descrFull': '',
+            'authorName': 'PyTest',
+            'authorEmail': 'test@mail.com',
+            'authorAff': 'PyTest'}
 
-        response = requests.request("POST", url, data=payload)
-        assert response.status_code == 200
+    def test_add_geneset(self):
+        response = geneset.add_geneset(self.form)
+        assert response == ('{"success": true}', 200, {'ContentType': 'application/json'})
