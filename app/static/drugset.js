bcebfbe4d84fb70b80cc9ab29674a189c86194d4
Maxim Kuleshov <kuleshov.max.v@gmail.com>
2020-06-17 20:43:34 -0400

    Increase delay before hiding a popover - fix #130

--- app/static/drugset.js
+++ app/static/drugset.js
@@ -65,6 +65,7 @@ function ds_drawTable(url, wrapper, reviewed, overlap_url, category = 0) {
                     'data-toggle': 'popover',
                     'data-placement': 'bottom',
                     'data-trigger': 'focus',
+                     'data-delay': '{ "show": 0, "hide": 4000 }',
                     'data-html': 'true',
                     'data-template': '<div class="popover enrichment-popover" role="tooltip"><div class="arrow"></div><h3 class="popover-header"></h3><div class="popover-body"></div></div>',
                     'data-content': desc,
